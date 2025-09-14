08f5d30620fe09752d991b54267bf58058b550ce
trungbq <trungbq86@gmail.com>
2025-09-14 10:39:41 +0700

    my course

--- src/layouts/DefaultLayout/components/MyCourses/MyCourse/index.jsx
+++ src/layouts/DefaultLayout/components/MyCourses/MyCourse/index.jsx
@@ -1,17 +1,19 @@
 import { Link } from 'react-router';
 import styles from './MyCourse.module.scss';
 
-function MyCourse({item}) {
+function MyCourse({ item }) {
+    console.log(item);
+    
     return (
         <div className={styles.container}>
             <div className={styles.item}>
                 <Link to={item.link}>
                     <div className={styles.left}>
-                        <img src={item.Image} alt={item.Name} />
+                        <img  className={styles.image} src={item.image} alt={item.name} />
                     </div>
                     <div className={styles.right}>
-                        <h3>{item.Name}</h3>
-                        <p>{item.Description}</p>
+                        <h3>{item.name}</h3>
+                        <p>{item.status}</p>
                     </div>
                 </Link>
             </div>
--- src/layouts/DefaultLayout/components/MyCourses/index.jsx
+++ src/layouts/DefaultLayout/components/MyCourses/index.jsx
@@ -12,6 +12,7 @@ function MyCourses() {
         {
             id: 1,
             link: '#',
+            image: 'https://files.fullstack.edu.vn/f8-prod/courses/31/67f4c93c28d4b.png',
             name: 'Fullstack Web Developer',
             status: 'Học cách đây 1 giờ trước',
             percent: 20
@@ -19,6 +20,7 @@ function MyCourses() {
         {
             id: 2,
             link: '#',
+            image: 'https://files.fullstack.edu.vn/f8-prod/courses/13/13.png',
             name: 'Xây dựng website với ReactJS',
             status: 'Bạn chưa học khóa học này',
             percent: 0
@@ -37,7 +39,20 @@ function MyCourses() {
                 </div>
 
                 <div className={clsx(styles.content, { [styles.open]: isOpen })}>
+
+
+
                     <Modal isOpen={isOpen}>
+
+                        {
+                            isOpen && (
+                                <div className={styles.header}>
+                                    <h2 className={styles.title}>Khóa học của tôi</h2>
+                                    <a href="#">Xem tất cả</a>
+                                </div>
+                            )
+                        }
+
                         {
                             courses.map((course) => (
                                 <MyCourse key={course.id} item={course} />
