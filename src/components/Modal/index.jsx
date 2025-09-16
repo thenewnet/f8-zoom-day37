60f151203367a0bc3f59a67bd8e42445917541ef
trungbq <trungbq86@gmail.com>
2025-09-16 21:07:56 +0700

    Modal

--- src/components/Modal/index.jsx
+++ src/components/Modal/index.jsx
@@ -1,5 +1,8 @@
+import { CircleX } from 'lucide-react';
 import styles from './Modal.module.scss';
 import PropTypes from "prop-types";
+import { useEffect } from 'react';
+import clsx from 'clsx';
 
 
 function Modal({
@@ -7,26 +10,110 @@ function Modal({
     onAfterOpen,
     onAfterClose,
     onRequestClose,
-    closeTimeoutMS,
+    closeTimeoutMS = 0,
     overlayClassName,
     className,
     bodyOpenClassName,
-    htmlOpenClassName,
-    shouldCloseOnOverlayClick,
-    shouldCloseOnEsc,
+    htmlOpenClassName = 'modal-open',
+    shouldCloseOnOverlayClick = true,
+    shouldCloseOnEsc = true,
     children }) {
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    const handleRequestClose = () => {
+        setTimeout(onRequestClose, closeTimeoutMS);
+    }
 
+    // For On Close
+    useEffect(() => {
+        const handleKeyup = (e) => {
+            if (shouldCloseOnEsc && e.key === 'Escape') {
+                handleRequestClose();
+            }
+        };
+
+        if (isOpen) {
+            document.addEventListener("keyup", handleKeyup);
+        }
+
+        return () => {
+            if (isOpen) {
+                document.removeEventListener("keyup", handleKeyup);
+            }
+        };
+    }, [isOpen, shouldCloseOnEsc, handleRequestClose]);
+
+
+    // For Body Open Class Name
+    useEffect(() => {
+        if (isOpen) {
+            document.body.classList.add(bodyOpenClassName);
+        }
+
+        return () => {
+            if (isOpen) {
+                document.body.classList.remove(bodyOpenClassName);
+            }
+        };
+    }, [isOpen, bodyOpenClassName]);
+
+
+    // For HTML Open Class Name
+    useEffect(() => {
+        if (isOpen) {
+            document.documentElement.classList.add(htmlOpenClassName);
+        }
+
+        return () => {
+            if (isOpen) {
+                document.documentElement.classList.remove(htmlOpenClassName);
+            }
+        };
+    }, [isOpen, htmlOpenClassName]);
+
+    // For onAfterOpen
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            if (isOpen && onAfterOpen) {
+                onAfterOpen();
+            }
+            if (!isOpen && onAfterClose) {
+                onAfterClose();
+            }
+        }, closeTimeoutMS);
+
+        return () => clearTimeout(timer);
+    }, [isOpen, onAfterOpen, onAfterClose, closeTimeoutMS]);
+
+    // For Render
     if (!isOpen) return null;
 
     return (
         <>
-            {
-                isOpen && (
-                    <div className={styles.container}>
+            <div className={styles.container}>
+                <div className={clsx(styles.content, className)}>
+                    {/* Close Button */}
+                    <button className={styles.closeBtn} onClick={handleRequestClose}>
+                        <CircleX />
+                    </button>
+
+                    {/* Body children */}
+                    <div className={styles.body}>
                         {children}
                     </div>
-                )
-            }
+                </div>
+
+                {/* Overlay */}
+                <div
+                    className={clsx(styles.overlay, overlayClassName)}
+                    onClick={() => {
+                        if (shouldCloseOnOverlayClick) {
+                            handleRequestClose();
+                        }
+                    }}
+                >
+
+                </div>
+            </div>
 
         </>
     )
--- src/pages/ModalDemo/index.jsx
+++ src/pages/ModalDemo/index.jsx
@@ -1,10 +1,119 @@
+import { useState } from 'react';
 import styles from './ModalDemo.module.scss';
+import Modal from '../../components/Modal';
 
 function ModalDemo() {
+    const [isOpenBasic, setIsOpenBasic] = useState(false);
+    const [isOpenAnimation, setIsOpenAnimation] = useState(false);
+    const [isOpenNotCloseOvelay, setIsOpenNotCloseOvelay] = useState(false);
+    const [isOpenNotCloseEsc, setIsOpenNotCloseEsc] = useState(false);
+    const [isOpenCustomClassName, setIsOpenCustomClassName] = useState(false);
+    const [isOpenCallback, setIsOpenCallback] = useState(false);
+
     return (
         <>
             <div className={styles.container}>
-                ModalDemo
+
+                <h1>Model demo page</h1>
+
+                {/* Basic Modal */}
+                <div className={styles.item}>
+                    <h5>Basic Modal</h5>
+                    <button className={styles.btnOpenModel} onClick={() => setIsOpenBasic(true)}>Open Basic Modal</button>
+
+                    <Modal isOpen={isOpenBasic} onRequestClose={() => setIsOpenBasic(false)}
+                    >
+                        <div>
+                            <h2>Basic Modal</h2>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quae.</p>
+                        </div>
+                    </Modal>
+                </div>
+
+                {/* Animation Modal */}
+                <div className={styles.item}>
+                    <h5>Modal với Animation</h5>
+                    <button className={styles.btnOpenModel} onClick={() => setIsOpenAnimation(true)}>Open Animation Modal</button>
+
+                    <Modal isOpen={isOpenAnimation}
+                        onRequestClose={() => setIsOpenAnimation(false)}
+                        closeTimeoutMS={300}
+                    >
+                        <div>
+                            <h2>Animation Modal</h2>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quae.</p>
+                        </div>
+                    </Modal>
+                </div>
+
+
+                <div className={styles.item}>
+                    <h5>Modal không đóng khi click overlay</h5>
+                    <button className={styles.btnOpenModel}
+                        onClick={() => setIsOpenNotCloseOvelay(true)}>Open Modal</button>
+                    <Modal isOpen={isOpenNotCloseOvelay}
+                        onRequestClose={() => setIsOpenNotCloseOvelay(false)}
+                        shouldCloseOnOverlayClick={false}
+                    >
+                        <div>
+                            <h2>Overlay Modal</h2>
+                            <p>Modal không đóng khi click overlay.</p>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quae.</p>
+                        </div>
+                    </Modal>
+                </div>
+                <div className={styles.item}>
+                    <h5>Modal không đóng khi nhấn Esc</h5>
+                    <button className={styles.btnOpenModel}
+                        onClick={() => setIsOpenNotCloseEsc(true)}>Open Modal</button>
+                    <Modal isOpen={isOpenNotCloseEsc}
+                        onRequestClose={() => setIsOpenNotCloseEsc(false)}
+                        shouldCloseOnEsc={false}
+                    >
+                        <div>
+                            <h2>Esc Modal</h2>
+                            <p>Modal không đóng khi click Esc.</p>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quae.</p>
+                        </div>
+                    </Modal>
+                </div>
+                <div className={styles.item}>
+                    <h5>Modal với custom className</h5>
+                    <button className={styles.btnOpenModel}
+                        onClick={() => setIsOpenCustomClassName(true)}
+                    >Open Modal</button>
+
+                    <Modal isOpen={isOpenCustomClassName}
+                        onRequestClose={() => setIsOpenCustomClassName(false)}
+                        className={styles.customModalClassName}
+                    >
+                        <div>
+                            <h2>Custom class Modal</h2>
+                            <p>Modal với custom className.</p>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quae.</p>
+                        </div>
+                    </Modal>
+                </div>
+                <div className={styles.item}>
+                    <h5>Modal với callbacks</h5>
+                    <button
+                        className={styles.btnOpenModel}
+                        onClick={() => setIsOpenCallback(true)}
+                    >
+                        Open Modal</button>
+                    <Modal isOpen={isOpenCallback}
+                        onRequestClose={() => setIsOpenCallback(false)}
+                        onAfterOpen={() => console.log('Modal opened')}
+                        onAfterClose={() => console.log('Modal closed')}
+                        closeTimeoutMS={300}
+                    >
+                        <div>
+                            <h2>Callbacks Modal</h2>
+                            <p>Modal với callbacks.</p>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quae.</p>
+                        </div>
+                    </Modal>
+                </div>
             </div>
         </>
     )
