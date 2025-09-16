83f9385091fd0f3aa4f0b11d55ff43677d811740
trungbq <trungbq86@gmail.com>
2025-09-16 08:55:01 +0700

    profile

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -1,10 +1,39 @@
+import { useEffect, useState } from 'react';
+
 import styles from './Profile.module.scss';
+import defaultAvatar from '../../assets/default_avatar.png';
 
 function Profile() {
+    const [previewAvatar, setPreviewAvatar] = useState(null);
+
+    useEffect(() => {
+        return () => {
+            URL.revokeObjectURL(previewAvatar);
+        }
+    }, [previewAvatar]);
+
+    const hadleChangeAvatar = (e) => {
+        const img = e.target.files[0];
+        const previewUrl = URL.createObjectURL(img);
+
+        setPreviewAvatar(previewUrl);
+    }
+
     return (
         <>
             <div className={styles.container}>
-                Profile
+                <div className={styles.avatarWrapper}>
+                    <h2>
+                        Profile
+                    </h2>
+
+                    <div>
+                        <label>
+                            <input type="file" accept='image/*' hidden onChange={hadleChangeAvatar} />
+                            <img src={previewAvatar || defaultAvatar} alt="avatar" className={styles.avatar} />
+                        </label>
+                    </div>
+                </div>
             </div>
         </>
     )
